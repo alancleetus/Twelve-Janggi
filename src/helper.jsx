86cc3531367c76a817342da1b15196cadde513a6
Alan Cleetus <alanclee95@gmail.com>
2024-07-20 17:03:59 -0700

    removing debug messages

--- src/helper.jsx
+++ src/helper.jsx
@@ -1,7 +1,5 @@
 const getChar = (i) => String.fromCharCode(i + 97);
 const getTileColor = (row, col) => {
-  console.log(row);
-  console.log(col);
   return (row + col) % 2 === 0 ? "tile tile-light" : "tile tile-dark";
 };
 
