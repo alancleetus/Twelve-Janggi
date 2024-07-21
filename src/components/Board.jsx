d222d4c172cb98ed09ccfae3db35e0140ac0d8ee
Alan Cleetus <alanclee95@gmail.com>
2024-07-20 17:03:50 -0700

    create row and col headers in board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,19 +5,31 @@ function Board() {
   const rows = [0, 1, 2, 3];
 
   return (
-    <>
+    <div className="board">
+      <div className="row-labels-grid">
+        {rows.map((row) => (
+          <div key={row} className="row-label">
+            {row}
+          </div>
+        ))}
+      </div>
       <div className="tiles">
         {rows.map((row) => {
           return cols.map((col) => {
             return (
-              <div key={row + col} className={getTileColor(row, col)}>
-                {row + " " + getChar(col)}
-              </div>
+              <div key={row + col} className={getTileColor(row, col)}></div>
             );
           });
         })}
       </div>
-    </>
+      <div className="col-labels-grid">
+        {cols.map((col) => (
+          <div key={col} className="col-label">
+            {getChar(col)}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
