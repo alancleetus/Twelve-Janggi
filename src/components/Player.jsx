55da6e786ff7ad4724e41d4f85c9f9cee4dffeed
Alan Cleetus <alanclee95@gmail.com>
2024-07-20 17:09:53 -0700

    minor style update to player area

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,14 +4,13 @@ const Player = ({ playerId }) => {
     <div className="player-area">
       <p>Player_{playerId}</p>
       <div className="player-box">
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
-        <div className="player-tile">test</div>
+        {/* <div className="player-tile">test</div>  */}
+        <div className="player-tile" draggable>
+          test
+        </div>
+        <div className="player-tile" draggable>
+          test
+        </div>
       </div>
     </div>
   );
