0cf14d97f43a913a606819f8a2d5e89a13e9e668
Alan Cleetus <alanclee95@gmail.com>
2024-07-21 17:47:05 -0700

    Calculate where pieces can move into

---
+++ src/arbiter/arbiter.jsx
@@ -0,0 +1,29 @@
+import {
+  getMinisterMoves,
+  getGeneralMoves,
+  getManMoves,
+  getKingMoves,
+  getFeudalLordMoves,
+  getCapturedMoves,
+} from "./getMoves";
+
+const arbiter = {
+  getRegularMoves: function ({ currPosition, piece, row, col }) {
+    if (piece.includes("Minister")) {
+      return getMinisterMoves({ currPosition, piece, row, col });
+    } else if (piece.includes("General")) {
+      return getGeneralMoves({ currPosition, piece, row, col });
+    } else if (piece.includes("Man")) {
+      return getManMoves({ currPosition, piece, row, col });
+    } else if (piece.includes("King")) {
+      return getKingMoves({ currPosition, piece, row, col });
+    } else if (piece.includes("Lord")) {
+      return getFeudalLordMoves({ currPosition, piece, row, col });
+    }
+  },
+  getCapturedMoves: function ({ currPosition }) {
+    return getCapturedMoves({ currPosition });
+  },
+};
+
+export default arbiter;
