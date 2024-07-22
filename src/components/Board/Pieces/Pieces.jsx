aaf38f420943ce8e5b8252bda60661b301ac8abd
Alan Cleetus <alanclee95@gmail.com>
2024-07-21 21:14:01 -0700

    refactoring code

--- src/components/Board/Pieces/Pieces.jsx
+++ src/components/Board/Pieces/Pieces.jsx
@@ -34,46 +34,43 @@ const Pieces = () => {
     return { x, y };
   };
 
-  const onDrop = (e) => {
+  const validateMove = (e) => {
     // position of piece being moved
     const [piece, row, col, index] = e.dataTransfer.getData("text").split(",");
 
     const newPosition = copyPosition(currPosition);
     const { x, y } = calculateCoords(e);
 
+    // if piece has movable tiles
     if (appState.candidateMoves?.find((m) => m[0] === x && m[1] === y)) {
       console.log(`Moving ${piece} from ${row}-${col} to ${x}-${y}`);
 
-      //when moving captured piece
+      //when moving captured piece, remove captured piece from captured list
       if (row == -1 && col == -1) {
-        if (piece.includes("white")) {
-          dispatch(
-            moveCaptured({
-              newCapturedList: currWhiteCaptured.filter((piece, i) => {
-                return index != i;
-              }),
-              actorColor: "white",
-            })
-          );
-        } else if (piece.includes("black")) {
-          dispatch(
-            moveCaptured({
-              newCapturedList: currBlackCaptured.filter((piece, i) => {
-                return index != i;
-              }),
-              actorColor: "black",
-            })
-          );
-        }
+        const actorColor = piece.includes("white") ? "white" : "black";
+        const newCapturedList = (
+          piece.includes("white") ? currWhiteCaptured : currBlackCaptured
+        ).filter((piece, i) => {
+          return index != i;
+        });
+
+        dispatch(
+          moveCaptured({
+            newCapturedList: newCapturedList,
+            actorColor: actorColor,
+          })
+        );
       }
+
       //moving board piece and not captured piece
-      if (row > -1 && col > -1) newPosition[row][col] = "";
-      newPosition[x][y] = piece;
+      if (row > -1 && col > -1) newPosition[row][col] = ""; //remove piece being moved from old position
+      newPosition[x][y] = piece; // place piece onto new position
 
       // if move location already has piece
       if (currPosition[x][y] !== "") {
         let capturedPiece = currPosition[x][y];
 
+        // when a piece gets captured it's color swaps
         if (capturedPiece.includes("white"))
           dispatch(
             capturePiece({
@@ -100,6 +97,11 @@ const Pieces = () => {
     }
 
     dispatch(clearCandidates());
+  };
+
+  const onDrop = (e) => {
+    e.preventDefault();
+    validateMove(e);
   };
 
   const dragOver = (e) => e.preventDefault();
